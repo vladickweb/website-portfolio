f58783fe6d7851bbcf5bda1181a1ad5c4ab29a8a
Vladyslav <vladickbaraza@gmail.com>
2021-10-30 23:05:30 +0200

    deploy

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,21 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Cover.css'
 import coverVideo from '../../media/coverVideo.mp4'
 
 const Cover = ({ language }) => {
-	const [ isPreload, setIsPreload ] = useState(false)
-
-	useEffect(() => {
-		const video = document.querySelector('.video')
-    
-		video.addEventListener('loadeddata', e => {
-			video.readyState >= 3 && setIsPreload(true)
-		})
-	}, [])
-
 	return (
 		<div className='cover-container'>
-    
 			<video className='video' src={coverVideo} autoPlay loop muted preload={'auto'} />
 			<h1>Vladyslav Kapkan</h1>
 			{language === 'en' ? <p>Full Stack Web Developer</p> : <p>Desarrollador Web Full Stack</p>}
